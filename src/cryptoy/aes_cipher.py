0f41cfd0a9fbe91d071bc3bbf4bebbf610babb66
404-Sk1ll3ss <164352670+404-Sk1ll3ss@users.noreply.github.com>
2024-08-05 09:31:19 +0200

    rendu

--- src/cryptoy/aes_cipher.py
+++ src/cryptoy/aes_cipher.py
@@ -1,13 +1,10 @@
-from cryptography.hazmat.primitives.ciphers.aead import (
-    AESGCM,
-)
-
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 def encrypt(msg: bytes, key: bytes, nonce: bytes) -> bytes:
-    # A implémenter en utilisant la class AESGCM
-    pass
+	aesgcm = AESGCM(key)
+	return aesgcm.encrypt(nonce, msg, None)
 
 
 def decrypt(msg: bytes, key: bytes, nonce: bytes) -> bytes:
-    # A implémenter en utilisant la class AESGCM
-    pass
+	aesgcm = AESGCM(key)
+	return aesgcm.decrypt(nonce, msg, None)
--- src/cryptoy/affine_cipher.py
+++ src/cryptoy/affine_cipher.py
@@ -1,80 +1,76 @@
-from math import (
-    gcd,
-)
+from math import gcd
 
-from cryptoy.utils import (
-    str_to_unicodes,
-    unicodes_to_str,
-)
-
-# TP: Chiffrement affine
-
+from cryptoy.utils import str_to_unicodes, unicodes_to_str
 
 def compute_permutation(a: int, b: int, n: int) -> list[int]:
-    # A implémenter, en sortie on doit avoir une liste result tel que result[i] == (a * i + b) % n
-    pass
-
+	return [(a * i + b) % n for i in range (0, n)]
 
 def compute_inverse_permutation(a: int, b: int, n: int) -> list[int]:
-    # A implémenter, pour cela on appelle perm = compute_permutation(a, b, n) et on calcule la permutation inverse
-    # result qui est telle que: perm[i] == j implique result[j] == i
-    pass
-
+	permutation = compute_permutation(a, b, n)
+	inv_permutation = [0]*n
+	for i in range(0, n):
+		inv_permutation[permutation[i]] = i
+	return inv_permutation
 
 def encrypt(msg: str, a: int, b: int) -> str:
-    # A implémenter, en utilisant compute_permutation, str_to_unicodes et unicodes_to_str
-    pass
-
+	unicodes =  str_to_unicodes(msg)
+	all_permutation = compute_permutation(a, b, 1114112)
+	permutation = [all_permutation[i] for i in unicodes]
+	return unicodes_to_str(permutation)
 
 def encrypt_optimized(msg: str, a: int, b: int) -> str:
-    # A implémenter, sans utiliser compute_permutation
-    pass
+	unicodes =  str_to_unicodes(msg)
+	permutation = [(a * i + b) % 1114112 for i in unicodes]
+	return unicodes_to_str(permutation)
 
 
 def decrypt(msg: str, a: int, b: int) -> str:
-    # A implémenter, en utilisant compute_inverse_permutation, str_to_unicodes et unicodes_to_str
-    pass
+	unicodes =  str_to_unicodes(msg)
+	all_inv_permutation = compute_inverse_permutation(a, b, 1114112)
+	inv_permutation = [all_inv_permutation[y] for y in unicodes]
+	return unicodes_to_str(inv_permutation)
 
 
 def decrypt_optimized(msg: str, a_inverse: int, b: int) -> str:
-    # A implémenter, sans utiliser compute_inverse_permutation
-    # On suppose que a_inverse a été précalculé en utilisant compute_affine_key_inverse, et passé
-    # a la fonction
-    pass
+	unicodes =  str_to_unicodes(msg)
+	inv_permutation = [(a_inverse*(y - b)) % 1114112 for y in unicodes]
+	return unicodes_to_str(inv_permutation)
 
 
 def compute_affine_keys(n: int) -> list[int]:
-    # A implémenter, doit calculer l'ensemble des nombre a entre 1 et n tel que gcd(a, n) == 1
-    # c'est à dire les nombres premiers avec n
-    pass
+	return [i for i in range(0, n) if gcd(i, n) == 1]
 
 
 def compute_affine_key_inverse(a: int, affine_keys: list, n: int) -> int:
-    # Trouver a_1 dans affine_keys tel que a * a_1 % N == 1 et le renvoyer
-    # Placer le code ici (une boucle)
-
-    # Si a_1 n'existe pas, alors a n'a pas d'inverse, on lance une erreur:
-    raise RuntimeError(f"{a} has no inverse")
+	for item in affine_keys:
+		if(a * item % n == 1):
+			return item
+	raise RuntimeError(f"{a} has no inverse")
 
 
 def attack() -> tuple[str, tuple[int, int]]:
-    s = "࠾ੵΚઐ௯ஹઐૡΚૡೢఊஞ௯\u0c5bૡీੵΚ៚Κஞїᣍફ௯ஞૡΚր\u05ecՊՊΚஞૡΚՊեԯՊ؇ԯրՊրր"
-    # trouver msg, a et b tel que affine_cipher_encrypt(msg, a, b) == s
-    # avec comme info: "bombe" in msg et b == 58
-
-    # Placer le code ici
-
-    raise RuntimeError("Failed to attack")
+	s = "࠾ੵΚઐ௯ஹઐૡΚૡೢఊஞ௯\u0c5bૡీੵΚ៚Κஞїᣍફ௯ஞૡΚր\u05ecՊՊΚஞૡΚՊեԯՊ؇ԯրՊրր"
+	for item in compute_affine_keys(58):
+		decrypted = decrypt(s, item, 58)
+		if ("bombe" in decrypted):
+			return (decrypted, (item, 58))
+	raise RuntimeError("Failed to attack")
 
 
 def attack_optimized() -> tuple[str, tuple[int, int]]:
-    s = (
-        "જഏ൮ൈ\u0c51ܲ೩\u0c51൛൛అ౷\u0c51ܲഢൈᘝఫᘝా\u0c51\u0cfc൮ܲఅܲᘝ൮ᘝܲాᘝఫಊಝ"
-        "\u0c64\u0c64ൈᘝࠖܲೖఅܲఘഏ೩ఘ\u0c51ܲ\u0c51൛൮ܲఅ\u0cfc\u0cfcඁೖᘝ\u0c51"
-    )
-    # trouver msg, a et b tel que affine_cipher_encrypt(msg, a, b) == s
-    # avec comme info: "bombe" in msg
-
-    # Placer le code ici
-
-    raise RuntimeError("Failed to attack")
+	s = (
+		"જഏ൮ൈ\u0c51ܲ೩\u0c51൛൛అ౷\u0c51ܲഢൈᘝఫᘝా\u0c51\u0cfc൮ܲఅܲᘝ൮ᘝܲాᘝఫಊಝ"
+		"\u0c64\u0c64ൈᘝࠖܲೖఅܲఘഏ೩ఘ\u0c51ܲ\u0c51൛൮ܲఅ\u0cfc\u0cfcඁೖᘝ\u0c51"
+	)
+	key_list = compute_affine_keys(1114112)
+	for count, value in enumerate(key_list):
+		print("a:", count, "/", len(key_list))
+		try:
+			value_1 = compute_affine_key_inverse(value, key_list, 1114112)
+		except:
+			continue
+		for index in range(1, 10001):
+			decrypted = decrypt_optimized(s, value_1, index)
+			if ("bombe" in decrypted):
+				return (decrypted, (value, index))
+	raise RuntimeError("Failed to attack")
